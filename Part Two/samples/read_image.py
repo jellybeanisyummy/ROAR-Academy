abe86255ced93be3a824b0757ab767dd236145f6
Laura Wang <lcyw2020@gmail.com>
2025-07-14 11:38:03 -0700

    added comments to read_image.py

--- Part Two/samples/read_image.py
+++ Part Two/samples/read_image.py
@@ -12,7 +12,7 @@ import os
 # Read an image file
 path = os.path.dirname(os.path.abspath(__file__))
 filename = path + '/' + 'lenna.bmp'
-data = image.imread(filename)
+data = image.imread(filename)       # specific for image files, returns a numpy array
 
 # Display image information
 print('Image type is: ', type(data))
@@ -23,11 +23,13 @@ plot_data = data.copy()
 for width in range(512):
     for height in range(10):
         plot_data[height][width] = [255, 0, 0]   # Alternatively plot_data[height][width][:] = [255, 0, 0]
-        plot_data[511-height][width] = [0,0,255]
+                                                 # changing first 10 rows to red
+        plot_data[511-height][width] = [0,0,255]    # changing last 10 rows to blue
 
 # Write the modified images
 image.imsave(path+'/'+'lenna-mod.jpg', plot_data)
 
 # use pyplot to plot the image
 pyplot.imshow(plot_data)
-pyplot.show()
+pyplot.show()       # pauses python so that it doesn't clear out the image immediately
+                    # this way we can see the image in a popup window
